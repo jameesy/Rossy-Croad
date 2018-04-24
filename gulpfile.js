d532ab2e06778ed658c832445f5a013d7bc099c2
James Bedford <james.bedford1991@gmail.com>
2018-04-24 08:32:05 +0100

    gulpfile build

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,12 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 
 gulp.task('default',function(){
     console.log('Gulp test');
 })
+
+gulp.task('style', function(){
+    gulp.src('sass/**/*.scss')
+        .pipe(sass()).on('error', sass.logError)
+        .pipe(gulp.dest('./css'))
+});
