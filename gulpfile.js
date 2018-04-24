18e720a7bc54b559a4ea4996a9a85e5bc396578b
James Bedford <james.bedford1991@gmail.com>
2018-04-24 15:13:21 +0100

    fix gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,7 @@ gulp.task('style', function(){
     gulp.src('sass/**/*.scss')
         .pipe(sass()).on('error', sass.logError)
         .pipe(autoprefixer({
-            browsers['last 2 versions']
+            browsers: ['last 2 versions']
         }))
         .pipe(gulp.dest('./css'))
 });
